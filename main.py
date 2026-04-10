e90d4d83e7363be7edc76bec7882a479bf5594e9
arnon-vongarnon <arnon.vongarnon@gmail.com>
2026-04-11 01:28:34 +0700

    Add Check hands

--- main.py
+++ main.py
@@ -9,23 +9,48 @@ ball_model = YOLO("./models/yolo11n.pt")
 
 # Video source (0 = webcam)
 # cap = cv.VideoCapture(0)
-cap = cv.VideoCapture("videos/test3.mp4")
+cap = cv.VideoCapture("videos/test2.mp4")
 
 while True:
     ret, frame = cap.read()
     if not ret:
         break
-    # Run pose detection
+    # Pose detection
     pose_results = pose_model(frame, verbose=False)
 
-    # Run object detection (ball)
+    # Ball detection
     ball_results = ball_model(frame, verbose=False)
 
     # Draw pose
     pose_frame = pose_results[0].plot()
 
-    # Draw ball detection on top
+    # Draw ball detection
     final_frame = ball_results[0].plot(img=pose_frame)
+
+    # Check hands
+    if pose_results[0].keypoints is not None and len(pose_results[0].keypoints.xy) > 0:
+        kpts = pose_results[0].keypoints.xy
+        person = kpts[0]
+
+        head = person[0]
+        left_wrist = person[9]
+        right_wrist = person[10]
+
+    threshold = 20
+
+    if left_wrist[1] < head[1] + threshold and right_wrist[1] < head[1] + threshold:
+        text = "Hands OK"
+        color = (0, 255, 0)
+    else:
+        text = "Hands too low"
+        color = (0, 0, 255)
+
+    cv.putText(final_frame, text, (50, 50),
+               cv.FONT_HERSHEY_SIMPLEX, 1, color, 2)
+
+    print("Head:", head)
+    print("Left wrist:", left_wrist)
+    print("Right wrist:", right_wrist)
 
     # Show result
     cv.imshow("Throw-in Checker", final_frame)
